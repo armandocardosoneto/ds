752f1c12e84bb4174f3b1c17dca365230cd87960
armandocardosoneto <armandocardos@gmail.com>
2023-10-17 00:19:50 +0000

    first commit

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+
+//Exercício 5
+var digitado = prompt('Digite um numero de 1 a 5.');
+
+if(digitado == 1){
+    alert('Numero um digitado.');
+}
+else if(digitado == 2){
+    alert('Numero dois digitado.');
+}
+else if(digitado == 3){
+    alert('Numero três digitado.');
+}
+else if(digitado == 4){
+    alert('Numero quatro digitado.');
+}
+else {
+    alert('Numero cinco digitado.');
+}
