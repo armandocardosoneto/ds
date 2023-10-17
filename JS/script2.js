f0702e3947e19f44b85be9b66f843c0db57b9216
armandocardosoneto <armandocardos@gmail.com>
2023-10-17 01:11:28 +0000

    third commit

--- JS/script2.js
+++ JS/script2.js
@@ -1,10 +1,9 @@
 var A=5;
-var A=4;
-var A=3;
-var A=6;
+var B=4;
+var C=3;
+var D=6;
 
-
-// V         v
+//    V        v
 if( A > C && C <= D )
 {
     console.log("Mensagem do operador e AND");
@@ -14,4 +13,21 @@ if( A > C && C <= D )
 if((A + B) > 10 || (A + B) == (C + D))
 {
     console.log("Mensagem do operador e OU")
+}
+
+//Exercício 7
+
+var letra = prompt("Digite uma letra: ");
+
+if(letra == "")
+{
+    alert("Erro. Valor Vazio.");
+}
+else if(letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u")
+{
+    alert("Letra digitada é uma vogal.");
+}
+else
+{
+    alert("Letra digitada é uma consoante.");
 }
