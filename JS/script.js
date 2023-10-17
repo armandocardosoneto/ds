1904ad9e0fd34835b3d1fe497c7b243d972a3bcb
armandocardosoneto <armandocardos@gmail.com>
2023-10-17 00:52:42 +0000

    second commit

--- JS/script.js
+++ JS/script.js
@@ -23,41 +23,42 @@ var digitado = prompt('digite um número de 1 a 100.');
 
 if(digitado <= 10)
 {
-    console.log("número está entre 1 e 10");
+    console.log("Valor digitado pelo usuário: " + digitado)
+    alert("número está entre 1 e 10");
 }
 else if(digitado <= 20)
 {
-    console.log("número está entre 11 e 20");
+    alert("número está entre 11 e 20");
 }
 else if(digitado <= 30)
 {
-    console.log("número está entre 21 e 30");
+    alert("número está entre 21 e 30");
 }
 else if(digitado <= 40)
 {
-    console.log("número está entre 31 e 40");
+    alert("número está entre 31 e 40");
 }
 else if(digitado <= 50)
 {
-    console.log("número está entre 41 e 50");
+    alert("número está entre 41 e 50");
 }
 else if(digitado <= 60)
 {
-    console.log("número está entre 51 e 60");
+    alert("número está entre 51 e 60");
 }
 else if(digitado <= 70)
 {
-    console.log("número está entre 61 e 70");
+    alert("número está entre 61 e 70");
 }
 else if(digitado <= 80)
 {
-    console.log("número está entre 71 e 80");
+    alert("número está entre 71 e 80");
 }
 else if(digitado <= 90)
 {
-    console.log("número está entre 81 e 90");
+    alert("número está entre 81 e 90");
 }
-else(digitado <= 20)
+else if(digitado <= 100)
 {
-    console.log("número está entre 91 100");
+    alert("número está entre 91 e 100");
 }
---
+++ JS/script2.js
@@ -0,0 +1,17 @@
+var A=5;
+var A=4;
+var A=3;
+var A=6;
+
+
+// V         v
+if( A > C && C <= D )
+{
+    console.log("Mensagem do operador e AND");
+}
+
+//      F       OU      V
+if((A + B) > 10 || (A + B) == (C + D))
+{
+    console.log("Mensagem do operador e OU")
+}
