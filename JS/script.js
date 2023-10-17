da70ecd24e98f08387ea997f55f5838f307b3fb3
armandocardosoneto <armandocardos@gmail.com>
2023-10-17 00:33:48 +0000

    second commit

--- JS/script.js
+++ JS/script.js
@@ -17,3 +17,47 @@ else if(digitado == 4){
 else {
     alert('Numero cinco digitado.');
 }
+
+//Exercício 6
+var digitado = prompt('digite um número de 1 a 100.');
+
+if(digitado <= 10)
+{
+    console.log("número está entre 1 e 10");
+}
+else if(digitado <= 20)
+{
+    console.log("número está entre 11 e 20");
+}
+else if(digitado <= 30)
+{
+    console.log("número está entre 21 e 30");
+}
+else if(digitado <= 40)
+{
+    console.log("número está entre 31 e 40");
+}
+else if(digitado <= 50)
+{
+    console.log("número está entre 41 e 50");
+}
+else if(digitado <= 60)
+{
+    console.log("número está entre 51 e 60");
+}
+else if(digitado <= 70)
+{
+    console.log("número está entre 61 e 70");
+}
+else if(digitado <= 80)
+{
+    console.log("número está entre 71 e 80");
+}
+else if(digitado <= 90)
+{
+    console.log("número está entre 81 e 90");
+}
+else(digitado <= 20)
+{
+    console.log("número está entre 91 100");
+}
